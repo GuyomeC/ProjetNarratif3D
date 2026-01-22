8ed98fd560af6b6d0ab7a8bb192efa6050168dda
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2026-01-22 11:27:37 +0100

    [END] proto

--- Assets/Scripts/PointAClickSystem.cs
+++ Assets/Scripts/PointAClickSystem.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PointAClickSystem : MonoBehaviour
 {
     [SerializeField] Camera MainCamera;
     [SerializeField] NavMeshAgent player;
 
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { 
-            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Mouse.current.leftButton.wasPressedThisFrame) { 
+            Vector3 mousePosition = Mouse.current.position.ReadValue();
+            Ray ray = MainCamera.ScreenPointToRay(mousePosition);
             RaycastHit hit;
 
             if(Physics.Raycast(ray, out hit))
